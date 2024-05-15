ed263b180eae9c33cbcffcbd8e5be8b03c741654
dylansote <148396201+dylansote@users.noreply.github.com>
2024-05-15 11:43:10 -0600

    Delete Terraform directory

--- Terraform/modules/network/outputs.tf
+++
@@ -1,7 +0,0 @@
-//poder exportar la propiedad, y todos los modulos puedan acceder a ella
-
-output "subnet_id" {
-    description = "Id de la subnet"
-    value = aws_subnet.subnet[0].id
-    
-}
--- Terraform/modules/network/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "vpc_name" {
-
-    type = string 
-    description = "nombre de la vpc desde el modulo" 
-
-}
-//solo funcionan en modulo
-
